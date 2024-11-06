1b5574061752ecf32e4ebbf6b3d836fa419cdd4b
Dmitry Egorov <info@consultapp.ru>
2024-11-06 16:45:45 +0300

    controlls +

--- src/Stoic.class.ts
+++ src/Stoic.class.ts
@@ -91,10 +91,13 @@ class Stoic {
             <backquote class="${this.params.baseClassName}__backquote">
             </backquote>
             <p class="${this.params.baseClassName}__author"></p>
-            <svg id="close" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-            <svg id="next" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>`;
+            <div class="${this.params.baseClassName}__controlls">
+              <svg id="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="transparent"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+                <path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+              <svg id="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-width="1.5" >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>
+            </div>
+`;
       this.quoteElement = this.params.root.querySelector(
         "backquote"
       ) as HTMLElement;
