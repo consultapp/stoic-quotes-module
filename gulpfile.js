ac0ad8cbac2b35dc9e7842596832409902d7a7f4
Dmitry Egorov <info@consultapp.ru>
2024-11-09 23:25:27 +0300

    gulp  start + css gulp

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// COMMON
+import gulp from "gulp";
+const { src, dest, watch, series } = gulp;
+
+import { deleteAsync } from "del";
+import syncServer from "browser-sync";
+const sync = syncServer.create();
+
+import flatten from "gulp-flatten";
+
+// // STYLE
+
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+
+import autoprefixer from "gulp-autoprefixer";
+import minify from "gulp-clean-css";
+import sourcemaps from "gulp-sourcemaps";
+import concat from "gulp-concat";
+
+// // HTML
+// import include from "gulp-file-include";
+
+// // IMAGES
+// import imagemin from "gulp-imagemin";
+
+const PACK_NAME = "stoic";
+
+function scss() {
+  return src("src/**/**.scss") // берем все файлы с расширением .scss
+    .pipe(sourcemaps.init()) // начинаем собирать sourcemap информацию
+    .pipe(sass().on("error", sass.logError)) // препроцессор sass
+    .pipe(autoprefixer()) // добавляем браузерные префиксы
+    .pipe(concat(`${PACK_NAME}.css`)) // собираем все в 1 файл
+    .pipe(minify()) // минифицируем
+    .pipe(sourcemaps.write(".")) // указываем sourcemap  туже директорию
+    .pipe(dest("./dist/css")); // сохраняем конечный фалй
+}
+
+function clear() {
+  return deleteAsync("dist"); // удаляем все файлы из итогового проекта
+}
+
+function serve() {
+  // инициализируем сервер, который хостит нашу папку итогового проекта
+  sync.init({ server: "./dist" });
+  // следим за файлами по маске и запускаем функции при их изменении
+  // далее перезагружаем сервер
+  // watch(["src/**/**.html"], series(html)).on("change", sync.reload);
+  watch(["src/**/**.scss"], series(scss)).on("change", sync.reload);
+  // watch(["src/assets/images/*"], series(images)).on("change", sync.reload);
+  // watch("src/**/**.js", series(js)).on("change", sync.reload);
+}
+
+const build = series([clear, scss]);
+const dev = series([clear, scss, serve]);
+// const build = series([clear, scss, html, js, images]);
+// const dev = series([clear, scss, html, js, images, serve]);
+
+export { build, dev };
