1db9a867bfaf6681836abace40800292d7251898
Dmitry Egorov <info@consultapp.ru>
2024-11-13 16:18:28 +0300

    controlls play/pause

--- src/components/StoicControlls/StoicControlls.tsx
+++ src/components/StoicControlls/StoicControlls.tsx
@@ -1,20 +1,44 @@
 import { ArrowRight, Pause, Play } from "lucide-react";
 import { Button } from "../ui/button";
 import styles from "./style.module.scss";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface Props {
   next: () => void;
 }
 
 export default function StoicControlls({ next }: Props) {
+  const [play, setPlay] = useState(true);
+  const timer = useRef<ReturnType<typeof setInterval>>();
+
+  const startTimer = useCallback(() => {
+    clearInterval(timer.current);
+    timer.current = setInterval(() => {
+      next();
+    }, 60 * 1000);
+  }, [next, timer]);
+
+  const stopTimer = useCallback(() => {
+    clearInterval(timer.current);
+  }, [timer]);
+
+  useEffect(() => {
+    if (play) startTimer();
+    else stopTimer();
+  }, [play, setPlay, startTimer, stopTimer]);
+
   return (
     <div className={styles.root}>
-      <Button variant={"ghost"} size="icon">
-        <Pause />
-      </Button>
-      <Button variant={"ghost"} size="icon">
-        <Play />
-      </Button>
+      {play && (
+        <Button variant={"ghost"} size="icon" onClick={() => setPlay(false)}>
+          <Pause />
+        </Button>
+      )}
+      {!play && (
+        <Button variant={"ghost"} size="icon" onClick={() => setPlay(true)}>
+          <Play />
+        </Button>
+      )}
       <Button variant="ghost" size="icon" onClick={next}>
         <ArrowRight />
       </Button>
